3a35a198154374fc21ea59bb471f3796d4104298
Robert P <porobertdev@gmail.com>
2024-10-06 10:17:45 +0300

    Validate user's input from login form

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,4 +1,20 @@
 const dotenv = require('dotenv').config();
+const validator = require('express-validator');
+
+const validateLogin = [
+    validator
+        .body('username')
+        .trim()
+        .notEmpty()
+        .isAlpha()
+        .withMessage("Username can't be empty!"),
+    validator
+        .body('password')
+        .trim()
+        .notEmpty()
+        .isLength({ min: 8, max: 32 })
+        .withMessage('Password must have between 8 and 32 chars!'),
+];
 
 module.exports = {
     get: (req, res) => {
@@ -6,25 +22,35 @@ module.exports = {
             title: 'Login',
         });
     },
-    post: (req, res) => {
-        console.log('wtf');
-        // res.render('login', {
-        //     title: 'Login',
-        // });
+    post: [
+        validateLogin,
+        (req, res) => {
+            const errors = validator.validationResult(req);
+            console.log('🚀 ~ errors:', errors);
 
-        const { username, password } = req.body;
-        console.log(username, password);
+            if (!errors.isEmpty()) {
+                res.status(400).render('index', {
+                    title: 'Errors occured',
+                    errors: errors.array(),
+                });
 
-        if (
-            username === process.env.ADMIN_USER &&
-            password === process.env.ADMIN_PASS
-        ) {
-            console.log('access granted as admin');
-            // res.send('Access granted. Redirecting...');
+                return;
+            }
 
-            setTimeout(() => res.redirect('/admin/panel'), 1000);
-        } else {
-            res.status(400).send('Access denied.');
-        }
-    },
+            // else continue
+            const { username, password } = req.body;
+            console.log(username, password);
+
+            if (
+                username === process.env.ADMIN_USER &&
+                password === process.env.ADMIN_PASS
+            ) {
+                console.log('access granted as admin');
+
+                setTimeout(() => res.redirect('/admin/panel'), 1000);
+            } else {
+                res.status(400).send('Access denied.');
+            }
+        },
+    ],
 };
