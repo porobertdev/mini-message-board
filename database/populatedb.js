41589ac364c7fa7f4682045c1f36e33072a2ef91
Robert P <porobertdev@gmail.com>
2024-10-06 21:57:30 +0300

    Add `populatedb` to create table if doesn't exist
    * Must be ran once with `node database/populatedb` 🤔

---
+++ database/populatedb.js
@@ -0,0 +1,42 @@
+const pool = require('./pool');
+
+const CREATE_SQL = `
+    CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, username VARCHAR (255), message TEXT);
+`;
+
+const INSERT_SQL = (username, message) =>
+    `INSERT INTO messages (username, message) VALUES ('${username}', '${message}')`;
+
+const placeholderMsg = [
+    {
+        username: 'porobertdev',
+        message: 'Hello there, just practicing nodejs and databases',
+    },
+
+    {
+        username: 'Robert',
+        message:
+            'I hate deadlifts, but love them at the same time. Does it make sense?',
+    },
+    {
+        username: 'random-dude',
+        message: 'Chuck Norris is cool',
+    },
+];
+
+async function populatedb() {
+    console.log('Populating DB...');
+
+    await pool.query(CREATE_SQL);
+
+    for (const m of placeholderMsg) {
+        const sql = INSERT_SQL(m.username, m.message);
+        await pool.query(sql);
+    }
+
+    await pool.end();
+
+    console.log('Done...');
+}
+
+populatedb();
